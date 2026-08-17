d6a3acb1cd4f3e7ad6a1612f1ef1a8f1e27fd005
Sanjana <divagar656@gmail.com>
2026-08-17 12:58:02 +0530

    Updated About information

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -38,11 +38,11 @@ const About = () => {
           <div className="about-sidebar animate-on-scroll from-right">
             <div className="about-stats">
               <div className="stat">
-                <h3>3+</h3>
+                <h3>4+</h3>
                 <p>Projects Completed</p>
               </div>
               <div className="stat">
-                <h3>2+</h3>
+                <h3>3+</h3>
                 <p>Years Learning</p>
               </div>
               <div className="stat">
